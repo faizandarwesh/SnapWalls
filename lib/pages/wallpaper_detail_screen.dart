513a7cb898a27747aca29197fcdb4099f7fcb688
FaizanGT <faizan.darwesh@genetechsolutions.com>
2024-12-06 15:25:31 +0500

    Add Hero Animation in Wallpaper Detail Screen

--- lib/pages/wallpaper_detail_screen.dart
+++ lib/pages/wallpaper_detail_screen.dart
@@ -1,10 +1,39 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-class WallpaperDetailScreen extends StatelessWidget {
-  const WallpaperDetailScreen({super.key});
+class WallpaperDetailPage extends StatelessWidget {
+  final String imageUrl;
+  final String tag;
+
+  const WallpaperDetailPage({
+    Key? key,
+    required this.imageUrl,
+    required this.tag,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.save))
+        ],
+      ),
+      body: Center(
+        child: Hero(
+          tag: tag, // Use the same tag as in WallpaperTile
+          child: CachedNetworkImage(
+            width: double.infinity,
+            height: double.infinity,
+            imageUrl: imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/wallpaper_tile.dart
+++ lib/widgets/wallpaper_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:snap_walls/pages/wallpaper_detail_screen.dart';
 
 class WallpaperTile extends StatelessWidget {
   final String imageUrl;
@@ -15,17 +16,24 @@ class WallpaperTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => WallpaperDetailPage(
+                    imageUrl: imageUrl, tag: index.toString())));
       },
-      child: Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-          clipBehavior: Clip.antiAlias,
-          height: extent,
-          child: CachedNetworkImage(
-            imageUrl: imageUrl,
-            fit: BoxFit.cover,
-          )),
+      child: Hero(
+        tag: index.toString(),
+        child: Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+            clipBehavior: Clip.antiAlias,
+            height: extent,
+            child: CachedNetworkImage(
+              imageUrl: imageUrl,
+              fit: BoxFit.cover,
+            )),
+      ),
     );
   }
 }
