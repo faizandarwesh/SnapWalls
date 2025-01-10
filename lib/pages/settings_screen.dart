5302158069a8a892dc416525ee8d47e38ffa0e53
FaizanGT <faizan.darwesh@genetechsolutions.com>
2025-01-10 17:10:12 +0500

    Add In App Review package to request for app rating from user

--- lib/pages/settings_screen.dart
+++ lib/pages/settings_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:in_app_review/in_app_review.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  final InAppReview inAppReview = InAppReview.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -52,18 +60,25 @@ class SettingsScreen extends StatelessWidget {
                   padding: const EdgeInsets.only(top: 32.0),
                   child: ListView(
                     children: [
-                      _buildSettingCard(Icons.star_border_outlined,
-                          'Rate the application', 'Your feedback is important'),
+                      _buildSettingCard(
+                          Icons.star_border_outlined,
+                          'Rate the application',
+                          'Your feedback is important', () async {
+                        if (await inAppReview.isAvailable()) {
+                          inAppReview.requestReview();
+                        }
+                      }),
                       _buildSettingCard(
                           Icons.share_outlined,
                           'Share with friends',
-                          'Tell your friends if you like it'),
+                          'Tell your friends if you like it',
+                          () {}),
                       _buildSettingCard(Icons.email_outlined, 'Contact Us',
-                          "Any questions? We'll help"),
+                          "Any questions? We'll help", () {}),
                       _buildSettingCard(Icons.security, 'Privacy Policy',
-                          'More about safety'),
-                      _buildSettingCard(
-                          Icons.sunny, 'Theme', 'Choose your preference', true),
+                          'More about safety', () {}),
+                      _buildSettingCard(Icons.sunny, 'Theme',
+                          'Choose your preference', () {}, true),
                     ],
                   ),
                 ),
@@ -76,39 +91,43 @@ class SettingsScreen extends StatelessWidget {
   }
 }
 
-Widget _buildSettingCard(IconData icon, String title, String subtitle,
+Widget _buildSettingCard(
+    IconData icon, String title, String subtitle, VoidCallback onPress,
     [bool isSwitch = false]) {
-  return Card(
-    color: const Color(0xffffffff),
-    elevation: 0,
-    margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(15),
+  return GestureDetector(
+    onTap: onPress,
+    child: Card(
+      color: const Color(0xffffffff),
+      elevation: 0,
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: ListTile(
+          leading: Container(
+              padding: const EdgeInsets.all(10.0),
+              decoration: const BoxDecoration(
+                  color: Color(0xfff2f3f5),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(Radius.circular(16))),
+              child: Icon(icon, color: Colors.black)),
+          title: Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          ),
+          subtitle: Text(
+            subtitle,
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+          ),
+          trailing: isSwitch
+              ? CupertinoSwitch(
+                  activeColor: const Color(0xfff2f3f5),
+                  value: true, // Here, you can bind it to a theme state
+                  onChanged: (value) {
+                    // Handle theme switching logic
+                  },
+                )
+              : const SizedBox()),
     ),
-    child: ListTile(
-        leading: Container(
-            padding: const EdgeInsets.all(10.0),
-            decoration: const BoxDecoration(
-                color: Color(0xfff2f3f5),
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.all(Radius.circular(16))),
-            child: Icon(icon, color: Colors.black)),
-        title: Text(
-          title,
-          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-        ),
-        subtitle: Text(
-          subtitle,
-          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-        ),
-        trailing: isSwitch
-            ? CupertinoSwitch(
-                activeColor: const Color(0xfff2f3f5),
-                value: true, // Here, you can bind it to a theme state
-                onChanged: (value) {
-                  // Handle theme switching logic
-                },
-              )
-            : const SizedBox()),
   );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import in_app_review
 import path_provider_foundation
 import sqflite_darwin
+import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
